0ceb2281861c3e70391f9f5320d71cf88fe62418
Serhii Reshetniak <reshet@lozovaya.org.ua>
2024-04-14 19:04:01 +0300

    Make buttons work

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-'use strict';
+(() => {
+  const mobileMenu = {
+    modal: document.querySelector('#mobile-menu'),
+    menuOpenBtn: document.querySelector('.button-menu-open'),
+    menuCloseBtn: document.querySelector('.modal-btn'),
+    menuCloseByLink: document.querySelector('.modal-menu-link'),
+    menuCloseByLinkShop: document.querySelector('.modal-menu-link-shop'),
+  };
 
-function openMobileMenu() {
-  const modal = document.getElementById('mobile-menu');
-  modal.classList.add('is-open');
-}
-
-function closeMobileMenu() {
-  const modal = document.getElementById('mobile-menu');
-  modal.classList.remove('is-open');
-}
+  mobileMenu.menuOpenBtn.addEventListener('click', toogleIsOpen);
+  mobileMenu.menuCloseBtn.addEventListener('click', toogleIsOpen);
+  mobileMenu.menuCloseByLink.addEventListener('click', toogleIsOpen);
+  mobileMenu.menuCloseByLinkShop.addEventListener('click', toogleIsOpen);
+  function toogleIsOpen() {
+    mobileMenu.modal.classList.toggle('is-open');
+  }
+})();
