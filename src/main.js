03f2da1121a8fc2c0a6d26e05ae9bb3d852ba4e5
Serhii Reshetniak <reshet@lozovaya.org.ua>
2024-04-14 19:22:07 +0300

    Fix closing menu by clicking a link

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
     modal: document.querySelector('#mobile-menu'),
     menuOpenBtn: document.querySelector('.button-menu-open'),
     menuCloseBtn: document.querySelector('.modal-btn'),
-    menuCloseByLink: document.querySelector('.modal-menu-link'),
+    menuCloseByLink: document.querySelectorAll('.modal-menu-link'),
     menuCloseByLinkShop: document.querySelector('.modal-menu-link-shop'),
   };
 
   mobileMenu.menuOpenBtn.addEventListener('click', toogleIsOpen);
   mobileMenu.menuCloseBtn.addEventListener('click', toogleIsOpen);
-  mobileMenu.menuCloseByLink.addEventListener('click', toogleIsOpen);
+  mobileMenu.menuCloseByLink.forEach(link => {
+    link.addEventListener('click', toogleIsOpen);
+  });
   mobileMenu.menuCloseByLinkShop.addEventListener('click', toogleIsOpen);
   function toogleIsOpen() {
     mobileMenu.modal.classList.toggle('is-open');
